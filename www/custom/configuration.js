27470b02fb8f5b7d438f533acdab592c7eaee055
Luis Gurmendez <luisgurmendez@gmail.com>
2017-09-20 23:02:52 -0300

    Added async sql callbacks compatibility as well as user save when joining group

--- www/custom/configuration.js
+++ www/custom/configuration.js
@@ -6,11 +6,11 @@ var configuration={};
 configuration.sql={};
 configuration.host="localhost"
 configuration.port="3000"
-configuration.sql.user="CREATE TABLE IF NOT EXISTS User (userId integer primary key autoincrement, username text, house text,userIdInServer text)";
+configuration.sql.user="CREATE TABLE IF NOT EXISTS User (userId integer primary key autoincrement, username text, house text,userIdInServer text unique)";
 configuration.sql.prediction="CREATE TABLE IF NOT EXISTS Prediction (predictionId integer primary key autoincrement, character text, status text, killedBy text, chapter integer, userId integer, FOREIGN KEY (userId) REFERENCES User(userId))";
 // Group es una palabra reservada de SQL de "Group By" hay que poner entra []
-configuration.sql.group="CREATE TABLE IF NOT EXISTS [Group] (groupId integer primary key autoincrement , groupIdInServer text ,name text, description text, numMembers integer, code text)";
-configuration.sql.userGroup="CREATE TABLE IF NOT EXISTS UserGroup (userId text, groupId text, PRIMARY KEY(userId,groupId), FOREIGN KEY (userId) REFERENCES User(userId), FOREIGN KEY (groupId) REFERENCES [Group](groupId))";
+configuration.sql.group="CREATE TABLE IF NOT EXISTS [Group] (groupId integer primary key autoincrement , groupIdInServer text unique,name text, description text, numMembers integer, code text)";
+configuration.sql.userGroup="CREATE TABLE IF NOT EXISTS UserGroup (userId text, groupId text, userIdInServer text,groupIdInServer text,  PRIMARY KEY(userId,groupId), FOREIGN KEY (userId) REFERENCES User(userId), FOREIGN KEY (groupId) REFERENCES [Group](groupId))";
 
 
 
